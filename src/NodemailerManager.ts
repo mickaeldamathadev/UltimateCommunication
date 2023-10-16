5915534d7a9b3779265576fc8a864b3d4b528975
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-10-16 12:23:58 +0200

    static mail

--- src/NodemailerManager.ts
+++ src/NodemailerManager.ts
@@ -1,9 +1,4 @@
-
-import mailer, {
-  SendMailOptions,
-  SentMessageInfo,
-  Transporter,
-} from "nodemailer";
+import mailer, { SendMailOptions, SentMessageInfo } from 'nodemailer'
 
 /**
  * @description A class to send mail using nodemailer
@@ -13,64 +8,59 @@ import mailer, {
  */
 
 export class Mail {
-  transporter: Transporter;
-  constructor() {
-    this.transporter = mailer.createTransport({
-      secure: true,
-      port: 465,
-      host: process.env.NODEMAILER_HOST,
-      auth: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS,
-      },
-    });
-  }
+  /**
+   * @description A function to send mail using nodemailer
+   * @param {string} receiver - target mail
+   * @param {string} subject - mail subject
+   * @param {string} body - mail content
+   * @param {string} attachment - attachment: { filename, path, contentType }
+   */
 
-
-    /**
- * @description A function to send mail using nodemailer
- * @param {string} receiver - target mail
- * @param {string} subject - mail subject
- * @param {string} body - mail content
- * @param {string} attachment - attachment: { filename, path, contentType }
- */
-
-  async send(
+  static async send(
     receiver: string,
     subject: string,
     body: string,
     attachment?: {
-            filename:string,
-            path: string,
-            contentType: "application/pdf" | "application/image",
-          },
+      filename: string
+      path: string
+      contentType: 'application/pdf' | 'application/image'
+    },
   ) {
     try {
-
+      const transporter = mailer.createTransport({
+        secure: true,
+        port: 465,
+        host: process.env.NODEMAILER_HOST,
+        auth: {
+          user: process.env.NODEMAILER_USER,
+          pass: process.env.NODEMAILER_PASS,
+        },
+      })
       const mailOptions: SendMailOptions = {
         from: process.env.NODEMAILER_USER, // sender address
         to: receiver, // list of receivers
         subject: subject, // Subject line
         html: body, // plain text body
-        attachments: [
-          attachment
-        ],
-      };
+        attachments: [attachment],
+      }
 
-      this.transporter.sendMail(
-        mailOptions,
-        function (err: Error | null, info: SentMessageInfo) {
-          if (err) {
-            throw new Error(err.message);
-          }
-        },
-      );
+      const mail = await new Promise((res, rej) => {
+        transporter.sendMail(
+          mailOptions,
+          function (err: Error | null, info: SentMessageInfo) {
+            if (err) {
+              rej(err)
+              throw new Error(err.message)
+            }
 
-      return {success:true}
+            res(info)
+          },
+        )
+      })
+
+      return { success: true, mail }
     } catch (error: any) {
-      return {error:error.message}
+      return { error: error.message }
     }
   }
 }
-
-
